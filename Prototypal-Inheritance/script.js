f02fd37d6a267f4af77f0e02d91caa2bc688e7ad
Alston <and_croning@hotmail.com>
2015-09-12 01:48:01 -0400

    Basic Prototypal inheritance demonstration

--- Prototypal-Inheritance/script.js
+++ Prototypal-Inheritance/script.js
@@ -8,10 +8,6 @@ function car(make, model, color) {
     this.make = make || "unknown";
     this.model = model || "model";
     this.color = color || "unpainted";
-    this.display = function() {
-        msg.innerHTML += "<p>Your car is a " + this.color + " " +
-        this.make + " " + this.model + ".</p>";
-    }
 }
 
 //create objects
@@ -19,7 +15,15 @@ var c1 = new car("Ford","Mustang","Blue");
 var c2 = new car("Ford","GTO","White");
 var c3 = new car();
 
+//this display methods now applies to every instance of the car object
+car.prototype.display = function() {
+    msg.innerHTML += "<p>Your car is a " + this.color + " " +
+    this.make + " " + this.model + ".</p>";
+};
+
+//change properties and run methods
 c2.color = "Red";
 c1.display();
 c2.display();
 c3.display();
+
