04ed0aae680e5da589add8945809721619c86619
Alston <and_croning@hotmail.com>
2015-09-29 21:49:02 -0400

    Preparing Files for Protypal Inheritence demonstration

---
+++ WarmUpPracticeThings/Scripts/PrototypalInheritence.js
@@ -0,0 +1,27 @@
+//Prototypal Inheritence
+
+//access 'message' element
+var msg = document.getElementById("message");
+
+//Car Constructor
+function Car (brand,model,color) {
+    this.brand = brand || "Unknown";
+    this.model = model || "Model";
+    this.color = color || "Unpainted";
+    this.display = function() {
+        msg.innerHTML += "<p>Your car is "+this.color+" "+this.brand+
+        " "+this.model+"</p>";
+    }
+}
+//Creating New Objects using Car Constructor
+var C1 = new Car("Ford","Mustang","Black");
+var C2 = new Car("Ford","GTO","White");
+var C3 = new Car();
+
+//Making Changes
+C2.color = "Blue";
+
+//Display Cars
+C1.display();
+C2.display();
+C3.display();
